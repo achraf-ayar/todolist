08d6d3baa9af22aa8aaa956356535f3d2562515b
Achraf Ayar <achrafayar00@gmail.com>
2026-01-26 09:59:30 +0100

    feat: seperate project models

--- projects.php
+++ projects.php
@@ -83,66 +83,7 @@ $projets = $stmt->fetchAll();
     <?php endif; ?>
 </div>
 
-
-<div class="modal fade" id="addProjectModal" tabindex="-1">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title"><i class="fas fa-folder-plus"></i> Nouveau Projet</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-            </div>
-            <form id="addProjectForm">
-                <div class="modal-body">
-                    <div class="mb-3">
-                        <label for="nom" class="form-label">Nom du projet *</label>
-                        <input type="text" class="form-control" id="nom" name="nom" required>
-                    </div>
-                    <div class="mb-3">
-                        <label for="couleur" class="form-label">Couleur</label>
-                        <input type="color" class="form-control form-control-color" id="couleur" name="couleur" value="#007bff">
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
-                    <button type="submit" class="btn btn-primary">
-                        <i class="fas fa-save"></i> Créer
-                    </button>
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
-
-
-<div class="modal fade" id="editProjectModal" tabindex="-1">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title"><i class="fas fa-edit"></i> Modifier Projet</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-            </div>
-            <form id="editProjectForm">
-                <input type="hidden" id="edit_id" name="id">
-                <div class="modal-body">
-                    <div class="mb-3">
-                        <label for="edit_nom" class="form-label">Nom du projet *</label>
-                        <input type="text" class="form-control" id="edit_nom" name="nom" required>
-                    </div>
-                    <div class="mb-3">
-                        <label for="edit_couleur" class="form-label">Couleur</label>
-                        <input type="color" class="form-control form-control-color" id="edit_couleur" name="couleur">
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
-                    <button type="submit" class="btn btn-warning">
-                        <i class="fas fa-save"></i> Modifier
-                    </button>
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
+<?php require_once 'includes/modals/project_modals.php'; ?>
 
 <script>
 
